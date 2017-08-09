2c8b9d6c17e75b8e2a92c03ec0f0a7c28cfcaf63
animorlus <whetley@hotmail.fr>
2017-08-09 08:37:16 -0400

    Moved the search bar to the main repo   - plugins.py :     - added the function get_recent_short_blog_posts     - added the function get_datasets     - now implementing ITemplateHelpers   - beautifulsoup4 is now required   - added the file canada_home.css as a resource   - added a simple search bar to the front page (index.html) (reverted from commit 3d49922bcebd32090c31ce1d40af4b4413e6c682)

--- ckanext/libraries/plugins.py
+++ ckanext/libraries/plugins.py
@@ -5,8 +5,6 @@ import json
 import ckan.plugins as p
 from ckan.lib.navl.dictization_functions import Missing
 
-from bs4 import BeautifulSoup
-
 
 def _dumps(o):
     if isinstance(o, Missing):
@@ -14,77 +12,15 @@ def _dumps(o):
     return json.dumps(o)
 
 
-def get_datasets():
-    datasets = p.toolkit.get_action('package_list')(
-        data_dict={}
-    )
-    return datasets
-
-
-def get_recent_short_blog_posts(number=10, exclude=None, length=250, suffix='...'):
-    """
-    Returns a shortened version of the ckanext-page content
-    type specified, limited by a specific size
-    :param exclude: Entry to be excluded
-    :param length: The maximum length of content allowed
-    :param number: The maximum number of elements to be returned
-    :return: An array of the objects (hashes), with the
-    parameter truncated added, specifying if the content add
-    to be truncated
-    """
-    blog_list = p.toolkit.get_action('ckanext_pages_list')(
-        data_dict={
-            'order_publish_date': True,
-            'private': False,
-            'page_type': 'blog'
-        }
-    )
-
-    entries = []
-
-    for blog_entry in blog_list:
-        if len(entries) >= number:
-            break
-        if exclude and blog_entry == exclude:
-            continue
-        # else the blog_entry is fetched and added to the list
-        # the contents can be html, so it needs to be converted to pure text
-        soup = BeautifulSoup(blog_entry['content'])
-        # removes titles, subtitles and lists from the text
-        for tag in ['h1', 'h2', 'h3', 'h4', 'h5' 'li', 'ul', 'ol']:
-            for x in soup.find_all(tag):
-                x.decompose()
-        blog_entry['content'] = soup.get_text()
-
-        if len(blog_entry['content']) > length:
-            # truncates to the last word inside the length,
-            # it shouldn't cut words
-            new_content = ' '.join((blog_entry['content'])[:length + 1 - len(suffix)]
-                                   .split(' ')[0:-1])
-            # since the previous operation is truncating the string to the right,
-            # the result string could potentially be the origin string
-            blog_entry['truncated'] = new_content[length - len(suffix):] != \
-                                      (blog_entry['content'])[length - len(suffix):]
-            blog_entry['content'] = new_content + suffix
-        else:
-            blog_entry['truncated'] = False
-
-        entries.append(blog_entry)
-
-    return entries
-
-
 class LibrariesPlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
     p.implements(p.IValidators)
     p.implements(p.IFacets)
     p.implements(p.IPackageController)
-    p.implements(p.ITemplateHelpers)
 
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'templates')
         p.toolkit.add_public_directory(config, 'public')
-        p.toolkit.add_resource('fanstatic', 'canada_libraries_theme')
 
     def get_validators(self):
         return {
@@ -152,8 +88,3 @@ class LibrariesPlugin(p.SingletonPlugin):
 
     def before_search(self, search_params):
         return search_params
-
-    def get_helpers(self):
-        return {
-            'datasets': get_datasets,
-        }
